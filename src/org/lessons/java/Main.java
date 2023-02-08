74ae11d4d34b3bcca6434483e0e04455c2bd56f5
Bice93 <bianca.cerulo@hotmail.it>
2023-02-08 17:24:01 +0100

    Bonus

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,5 +1,6 @@
 package org.lessons.java;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -56,6 +57,97 @@ public class Main {
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
+		
+		
+		
+	/*
+		 * 	BONUS
+	*/
+		
+		// tutte le lingue parlate in quella country
+		
+	/*	try (Connection con = DriverManager.getConnection(url, user, password)) {
+			System.out.println("Connessione al database effettuata");
+			
+			Scanner s = new Scanner(System.in);
+			System.out.println("Inserisci l'id di un paese: ");
+			int inputUser = Integer.parseInt(s.nextLine());
+			s.close();
+			
+			//Scrivere la query sql
+			String sql = "SELECT l.`language` \r\n"
+					+ "FROM countries c \r\n"
+					+ "INNER JOIN country_languages cl \r\n"
+					+ "	ON cl.country_id = c.country_id \r\n"
+					+ "INNER JOIN languages l \r\n"
+					+ "	ON l.language_id = cl.language_id\r\n"
+					+ "WHERE c.country_id = ?";
+			
+			try (PreparedStatement ps=con.prepareStatement(sql)){
+				//posso utilizzare il preparestatement (istruzione che devo eseguire)
+				
+				ps.setInt(1, inputUser);
+
+					try(ResultSet rs = ps.executeQuery()) {
+						//posso utilizzare resultSet (risultato dell'istruzione)
+
+						while (rs.next()) { //fintanto c'è qualcosa da leggere
+							String lenguage = rs.getString(1);
+							System.out.println("Lenguage: " + lenguage + "\t");
+						
+						}
+					}
+			}
+			
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} */ 
+		
+		
+		// le statistiche più recenti per quella country
+		
+	/*	try (Connection con = DriverManager.getConnection(url, user, password)) {
+			System.out.println("Connessione al database effettuata");
+			
+			Scanner s = new Scanner(System.in);
+			System.out.println("Inserisci l'id di un paese: ");
+			int inputUser = Integer.parseInt(s.nextLine());
+			s.close();
+			
+			//Scrivere la query sql
+			String sql = "SELECT c.country_id, c.name, cs.population, cs.gdp, cs.`year` AS anno\r\n"
+					+ "FROM countries c \r\n"
+					+ "INNER JOIN country_stats cs \r\n"
+					+ "ON cs.country_id = c.country_id \r\n"
+					+ "WHERE c.country_id = ? \r\n"
+					+ "ORDER BY anno DESC\r\n"
+					+ "LIMIT 2;";
+			
+			try (PreparedStatement ps=con.prepareStatement(sql)){
+				//posso utilizzare il preparestatement (istruzione che devo eseguire)
+				
+				ps.setInt(1, inputUser);
+
+					try(ResultSet rs = ps.executeQuery()) {
+						//posso utilizzare resultSet (risultato dell'istruzione)
+						
+						while (rs.next()) { //fintanto c'è qualcosa da leggere
+							
+							int idCountry = rs.getInt(1);
+							String nameCountry = rs.getString(2);
+							int population = rs.getInt(3);
+							BigDecimal gdp = rs.getBigDecimal(4);
+							int year = rs.getInt(5);
+							System.out.println("Statistiche: " + nameCountry);
+							System.out.println("Id_Country: " + idCountry + "\t Country: " + nameCountry + "\t Population: " + population + "\t Gdp: " + gdp + "\t year: " + year + "\n");
+						
+						}
+					}
+			}
+			
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} */
 	}
 	
 }
